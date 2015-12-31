0ddc3c2b56cb5069f8d105e31aba3cf00502bf1a
Michael Brown <gamer5108@hotmail.com>
2015-12-30 23:04:03 -0700

    Add GameManager to handle restart and quit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager instance_;
+    public static GameManager Instance
+    {
+        get
+        {
+            if(instance_ == null)
+            {
+                instance_ = FindObjectOfType<GameManager>();
+                if(instance_ == null)
+                {
+                    var go = new GameObject("GameManager (Instantiated)");
+                    instance_ = go.AddComponent<GameManager>();
+                }
+            }
+
+            return instance_;
+        }
+    }
+
+    public readonly float GRAVITY = 9.81f;
+
+    private void Update()
+    {
+        if (Input.GetButton("Restart"))
+        {
+            Player.Instance.SaveRecording();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetButton("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,6 @@ using System.Collections;
 [RequireComponent (typeof(Record))]
 public class Player : PlayerBase {
 
-    static readonly float GRAVITY = 9.81f;
     public float movementSpeed = 5;
     public float turningSpeed = 60;
     public float jumpForce = 8;
@@ -14,6 +13,20 @@ public class Player : PlayerBase {
 
     private CharacterController cc;
     private Record recording;
+
+    private static Player instance_;
+    public static Player Instance
+    {
+        get
+        {
+            if(instance_ == null)
+            {
+                instance_ = FindObjectOfType<Player>().GetComponent<Player>();
+            }
+
+            return instance_;
+        }
+    }
 
     protected override void Start()
     {
@@ -29,10 +42,10 @@ public class Player : PlayerBase {
         if(cc.isGrounded)
             gravity = 0f;
 
-        gravity -= GRAVITY * Time.deltaTime;
+        gravity -= GameManager.Instance.GRAVITY * Time.deltaTime;
 
         //Jump
-        if (Input.GetKeyDown("space") && cc.isGrounded)
+        if (Input.GetButton("Jump") && cc.isGrounded)
         {
             gravity += jumpForce;
             recording.RegisterAction("jump");
@@ -43,12 +56,6 @@ public class Player : PlayerBase {
 
         Vector3 vertical = Input.GetAxis("Vertical") * transform.forward * movementSpeed * Time.deltaTime;
         Move((x) => cc.Move(x), vertical + gravity*Vector3.up*Time.deltaTime);
-
-        if (Input.GetKeyDown("r"))
-        {
-            SaveRecording();
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
     }
 
     public void SaveRecording()
