304c8d10b1ee458e419711d6d23d6e081e963cd3
Utsav Jamwal <nick.utsav2345@gmail.com>
2016-01-08 22:53:04 -0700

    Time medallion, picking it up and keeping a list of medallion pieces

---
+++ Assets/Scripts/TimeMedallion.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class TimeMedallion : MonoBehaviour {
+    //private int i = 0;
+    //Vector3 vector = new Vector3(0, 0, 0);
+    public List<Vector3> index = new List<Vector3>();
+    GameObject[] medallions;
+
+    void OnCollisionEnter(Collision collision)
+    {
+       if (collision.gameObject.tag == "Player" )
+        {
+            Destroy(gameObject);
+            Player.Instance.SaveRecording();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+       
+    }
+	// Use this for initialization
+	void Start () {
+
+        medallions = GameObject.FindGameObjectsWithTag("Medallion");
+        foreach(GameObject item in medallions)
+        {
+            //index[i] = item.transform.position;
+            //i += 1;
+            print(item);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+}
