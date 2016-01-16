20a2381aa139d33d43f97adaa953772e047860a4
Siddardha Vangala <u1015410@ad.utah.edu>
2016-01-16 16:23:21 -0700

    Added Main Menu and Rotating effect

---
+++ Assets/Scripts/StartGameMainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGameMainMenu : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKey && !Input.GetButton("Cancel"))
+        {
+            PersistentManager.Instance.ResetData();
+            SceneManager.LoadScene("YueTestLevel");
+        }
+        if (Input.GetButton("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+}
