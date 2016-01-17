1725fde45626c2bd6af4c5d2a35858dc7f8549e6
Michael Brown <gamer5108@hotmail.com>
2016-01-16 19:00:25 -0700

    Add camera move forward against wall

--- Assets/Scripts/CameraScripts/MouseAimCamera.cs
+++ Assets/Scripts/CameraScripts/MouseAimCamera.cs
@@ -5,10 +5,31 @@ public class MouseAimCamera : MonoBehaviour {
 	public GameObject target;
 	public float rotateSpeed = 5;
 	Vector3 offset;
+    float startLength;
+    float length;
+    int layerMask;
 	
 	void Start() {
 		offset = target.transform.position - transform.position;
+        length = offset.magnitude;
+        startLength = length;
+        offset.Normalize();
+        layerMask = 1 << LayerMask.NameToLayer("Player");
 	}
+
+    private void FixedUpdate() {
+        RaycastHit hit;
+        Vector3 targetToUs = transform.position - target.transform.position;
+        if(Physics.Raycast(target.transform.position, targetToUs, out hit, startLength, ~layerMask))
+        {
+            length = Vector3.Distance(hit.point, target.transform.position);
+        }
+        else
+        {
+            length = startLength;
+        }
+    }
+
 	
 	void LateUpdate() {
 		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
@@ -16,7 +37,7 @@ public class MouseAimCamera : MonoBehaviour {
 
 		float desiredAngle = target.transform.eulerAngles.y;
 		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-		transform.position = target.transform.position - (rotation * offset);
+		transform.position = target.transform.position - length*(rotation * offset);
 		
 		transform.LookAt(target.transform);
 	}
